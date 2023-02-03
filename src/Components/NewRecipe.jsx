d12cc5e6863c5e180ecde4137bbfb9996f75ea8a
Marina <78799773+marinezh@users.noreply.github.com>
2023-02-03 02:16:52 +0200

    fix adding ingredients issue in newRecipe component

--- src/Components/NewRecipe.jsx
+++ src/Components/NewRecipe.jsx
@@ -10,9 +10,23 @@ const NewRecipe = () => {
     country: "",
     description: "",
     image: "",
-    ingredients: [{ ingredient: "", quantity: "" }],
+    ingredients: [],
     instructions: "",
   });
+
+  const [addIngredient, setAddIng] = useState([
+    { quantity: "", unit: "", ingredient: "" },
+  ]);
+
+  const addHandler = () => {
+    setAddIng([...addIngredient, { quantity: "", unit: "", ingredient: "" }]);
+  };
+
+  const addNewIngredient = (event, index) => {
+    [...addIngredient][index][event.target.name] = event.target.value;
+    setAddIng([...addIngredient]);
+    setNewRecipe({ ...newRecipe, ingredients: addIngredient });
+  };
 
   const changeHandler = (event) => {
     setNewRecipe({ ...newRecipe, [event.target.name]: event.target.value });
@@ -28,64 +42,96 @@ const NewRecipe = () => {
   return (
     <div className={classes.new_recipe_wrap}>
       <h1>Add new recipe here</h1>
-      <div>
+      <div className={classes.input}>
         <label htmlFor="name">Name</label>
-        <input onChange={changeHandler} type="text" id="name" name="name" />
+        <input
+          onChange={(e) => changeHandler(e)}
+          type="text"
+          id="name"
+          name="name"
+        />
       </div>
-      <div>
+      <div className={classes.input}>
         <label htmlFor="author">Author</label>
-        <input onChange={changeHandler} type="text" id="author" name="author" />
+        <input
+          onChange={(e) => changeHandler(e)}
+          type="text"
+          id="author"
+          name="author"
+        />
       </div>
-      <div>
+      <div className={classes.input}>
         <label htmlFor="country">Country</label>
         <input
-          onChange={changeHandler}
+          onChange={(e) => changeHandler(e)}
           type="text"
           id="country"
           name="country"
         />
       </div>
-      <div>
+      <div className={classes.input}>
         <label htmlFor="description">Description</label>
-        <input
-          onChange={changeHandler}
+        <textarea
+          rows="4"
+          cols="50"
           type="text"
           id="description"
           name="description"
+          onChange={(e) => changeHandler(e)}
+        ></textarea>
+      </div>
+      <div className={classes.input}>
+        <label htmlFor="img">Image</label>
+        <input
+          onChange={(e) => changeHandler(e)}
+          type="text"
+          id="img"
+          name="image"
         />
       </div>
       <div>
-        <label htmlFor="img">Image</label>
-        <input onChange={changeHandler} type="text" id="img" name="image" />
-      </div>
-      <div>
         <p>Ingredients</p>
-        <div>
-          <div>
-            <label htmlFor="quantity">Quantity</label>
-            <input
-              onChange={changeHandler}
-              type="text"
-              id="quantity"
-              name="quantity"
-            />
+        {addIngredient.map((ingredient, index) => (
+          <div key={index} className={classes.ing_inputs}>
+            <div className={classes.input1}>
+              <label htmlFor="quantity">Quantity</label>
+              <input
+                onChange={(e) => addNewIngredient(e, index)}
+                type="text"
+                id="quantity"
+                name="quantity"
+                value={ingredient.quantity}
+              />
+            </div>
+            <div className={classes.input2}>
+              <label htmlFor="input">Input</label>
+              <input
+                onChange={(e) => addNewIngredient(e, index)}
+                type="text"
+                id="unit"
+                name="unit"
+                value={ingredient.unit}
+              />
+            </div>
+            <div className={classes.input3}>
+              <label htmlFor="ingredient">Ingredient</label>
+              <input
+                onChange={(e) => addNewIngredient(e, index)}
+                type="text"
+                id="ingredient"
+                name="ingredient"
+                value={ingredient.ingredient}
+              />
+            </div>
           </div>
-          <div>
-            <label htmlFor="ingredient">Ingredient</label>
-            <input
-              onChange={changeHandler}
-              type="text"
-              id="ingredient"
-              name="ingredient"
-            />
-          </div>
-        </div>
-        <button>Add more</button>
+        ))}
+
+        <button onClick={addHandler}>Add more</button>
       </div>
-      <div>
+      <div className={classes.input}>
         <label htmlFor="instr">Instructions</label>
         <textarea
-          onChange={changeHandler}
+          onChange={(e) => changeHandler(e)}
           id="instr"
           rows="4"
           cols="50"
